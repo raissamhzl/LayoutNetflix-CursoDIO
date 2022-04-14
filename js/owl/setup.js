a83cddac6807ecb9c9d2452b79266a7c81fab70c
raissamhzl <raissamunhoz4@gmail.com>
2022-04-14 18:04:52 -0300

    layout final

---
+++ js/owl/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        500:{
+            items:3
+        },
+        700:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+}) 
